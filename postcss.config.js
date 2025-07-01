1489eee16ee1db45de121650b9fae993b4ba4154
vikasco987 <vikasvikas9095@gmail.com>
2025-07-01 16:05:32 +0530

    add

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
+
+
+
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
